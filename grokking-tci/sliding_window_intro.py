7a2a4992d8fa46f2eab032b3aff94b133192ba80
Hitesh Goel <hitesh@udacity.com>
2020-03-02 20:35:32 +0530

    M .vscode/settings.json M  grokking-tci/sliding_window_intro.py ?? dailycodingproblems/315.py ?? dailycodingproblems/solutions/

--- grokking-tci/sliding_window_intro.py
+++ grokking-tci/sliding_window_intro.py
@@ -1,18 +1,17 @@
 def main():
     arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
     k = 5
-    sum = 0
-    
-    for i in range(k):
-        sum = sum + arr[i]
-    
-    avg_arr = []
+    win_sum, win_start, arr_avg = 0, 0, []
 
-    for i in range(k, len(arr) + 1):
-        temp = sum/k
-        avg_arr.append(temp)
-        if i < len(arr):
-            sum = sum + arr[i] - arr[i-k]
+    for win_end in range(len(arr)):
+        win_sum += arr[win_end]
+        if win_end >= k-1:
+            arr_avg.append(win_sum/k)
+            win_sum -= arr[win_start]
+            win_start += 1
+    
+    return arr_avg
+    
     
     return avg_arr 
 
